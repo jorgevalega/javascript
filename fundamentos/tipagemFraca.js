c2620c56b178e1d0191ce557662257c01533d698
Jorge Valega <jorgevalega@gmail.com>
2019-08-03 09:49:17 -0300

    Create tipagemFraca.js
    Tipagem fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,14 @@
+// Javascript é de tipagem fraca, tipagem dinâmica.
+
+let varLetTeste1 = 'Teste'
+console.log(varLetTeste1)
+console.log(typeof varLetTeste1)
+
+varLetTeste1 = 1.2345
+console.log(varLetTeste1)
+console.log(typeof varLetTeste1)
+
+// evitar nomes genéricos e siglas
+let valor = ''
+let numero = 123
+let pqp = false // produto químico perigoso kkk
