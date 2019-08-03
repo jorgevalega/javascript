48a281fc0316244e3820ed156442bcb9c861f278
Jorge Valega <jorgevalega@gmail.com>
2019-08-03 09:03:05 -0300

    Create variaveisEConstantes.js
    Variaveis e Constantes

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,18 @@
+let varLet1 = 1
+var varVar1 = 2
+
+// let varLet1 = 10  // o identificador varLet1 já foi declarado
+varLet1 = 10
+var varVar1 = 20
+
+console.log(varLet1, varVar1)
+
+varLet1 = 100
+varVar1 = 200
+
+console.log(varLet1, varVar1)
+
+const constTeste1 = 3
+// const constTeste1 = 30 // não pode fazer uma atribuição para uma constante
+
+console.log(constTeste1)
