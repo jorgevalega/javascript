4af20d9bc322af686d00266fe485f6da75ba1d51
Jorge Valega <jorgevalega@gmail.com>
2019-08-03 14:35:19 -0300

    Update numeros.js
    Atualização Números

--- fundamentos/numeros.js
+++ fundamentos/numeros.js
@@ -14,3 +14,14 @@ console.log(Number.isInteger(peso3))
 
 console.log(peso4)
 console.log(Number.isInteger(peso4))
+
+const avaliacao1 = 9.876
+const avaliacao2 = 6.654
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso3 
+const media = total / (peso1 + peso3)
+
+console.log(media.toFixed(2))
+console.log(media.toString(2)) // em binário
+console.log(typeof media)
+console.log(typeof Number)
