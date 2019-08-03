45602434da87cfec2307415306ffb437f1b78f44
Jorge Valega <jorgevalega@gmail.com>
2019-08-03 14:21:15 -0300

    Create numeros.js
    Números

---
+++ fundamentos/numeros.js
@@ -0,0 +1,16 @@
+const peso1 = 1.0
+const peso2 = 1.1
+const peso3 = Number('2.0')
+const peso4 = Number('2.2')
+
+console.log(peso1)
+console.log(Number.isInteger(peso1))
+
+console.log(peso2)
+console.log(Number.isInteger(peso2))
+
+console.log(peso3)
+console.log(Number.isInteger(peso3))
+
+console.log(peso4)
+console.log(Number.isInteger(peso4))
