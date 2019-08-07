4b6c5b7ffd96665a813dfc15acd1d48a5b6d5610
Jorge Valega <jorgevalega@gmail.com>
2019-08-06 21:34:53 -0300

    Create numerosAlgunsCuidados.js
    Alguns Cuidados com números

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,12 @@
+
+console.log(5 / 0) // em Javascript existe o tipo llamado infinity
+
+console.log('10' / 2)
+console.log('10.2' / 2)
+console.log('10,2' / 2)
+console.log('Show' * 2)
+
+console.log(0.1 + 0.7) // impresição
+
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
