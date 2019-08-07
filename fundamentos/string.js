644e98446c0be0630f713db59c09f9140e5381a7
Jorge Valega <jorgevalega@gmail.com>
2019-08-07 11:29:57 -0300

    Update string.js
    Modificações no arquivo String

--- fundamentos/string.js
+++ fundamentos/string.js
@@ -14,3 +14,12 @@ console.log(marcaCarro.substring(0, 3)) // não inclui o íncide 3
 
 console.log('Marca carro: '.concat(marcaCarro).concat('!')) // função de concatenar
 console.log('A marca do carro é: '.concat(marcaCarro))
+
+console.log(marcaCarro.replace('a', 4))
+console.log(marcaCarro.replace('o', 0))
+console.log(marcaCarro.replace(/o/g, 0)) // substutui todas as letras o pelo número 0
+console.log(marcaCarro.replace(/t/g, 7)) // não subistitui as maiúsculas
+console.log(marcaCarro.replace(/t/gi, 7)) // subistitui todas as letras t 
+
+console.log('Ana,Maria,Pedro'.split(',')) // a vírgula é o separador, virou um array
+console.log('Ana,Maria,Pedro'.split(/,/)) // mesma coisa que o anterior com RegEx
