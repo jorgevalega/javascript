78131a88ff1615e24202c7feeec785b7d55da6dc
Jorge Valega <jorgevalega@gmail.com>
2019-08-09 09:36:09 -0300

    Update objetos.js
    Atualização Objetos

--- fundamentos/objetos.js
+++ fundamentos/objetos.js
@@ -4,3 +4,10 @@ prod1.preco = 4999.90
 prod1['Desconto legal'] = 0.40 // evitar atributos com espaços
 
 console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+
+console.log(prod2)
