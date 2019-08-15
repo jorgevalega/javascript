fe5c16644f0f2cc452d30e702e934c2bd5dbe133
Jorge Valega <jorgevalega@gmail.com>
2019-08-15 08:16:40 -0300

    Create usandoVar1.js
    Criação de usandoVar1.js

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,19 @@
+{
+    {
+        {
+            {
+                var teste1 = 'Algúm conteúdo'
+                console.log(teste1)
+            }
+        }
+    }
+}
+console.log(teste1)
+
+function teste() {
+    var local = 123 // variável dentro de uma função
+    console.log(local)
+}
+
+teste()
+// console.log(local) // a variável só é visivel dentro da function
