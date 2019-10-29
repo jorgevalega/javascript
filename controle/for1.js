66ca3ed1beeccf06c48e6a6016d48b2bb7e877bb
Jorge Valega <jorgevalega@gmail.com>
2019-10-28 21:38:40 -0300

    Create for1.js
    Criação de for1.js

---
+++ controle/for1.js
@@ -0,0 +1,14 @@
+let contador = 1
+while (contador <= 10) {
+    console.log(`Contador = ${contador}`)
+    contador++
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+const notas = [6.7, 8.2, 9.4, 5.9, 7.1]
+for (i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`)
+}
