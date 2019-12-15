bda02c111e6ecb98d9fb3b5b99be52e3d849761e
Jorge Valega <jorgevalega@gmail.com>
2019-12-15 08:55:34 -0300

    Create tiposDeclaracao.js
    Tipos de declaração de funções

---
+++ funcao/tiposDeclaracao.js
@@ -0,0 +1,20 @@
+console.log(soma(3, 5))
+
+// function declaration
+function soma(x, y) {
+    return x + y
+}
+
+// funcion expression
+const sub = function (x, y) {
+    return x - y
+}
+
+console.log(sub(5, 9))
+
+// named function expression (pouco usado)
+const mult = function mult(x, y) {
+    return x * y
+}
+
+console.log(mult(3, 7))
